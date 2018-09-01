7b9823028197ceed898e733c09d04bdfc4492a59
Sylvester Okonkwo <theslyguy@icloud.com>
2018-09-01 23:37:10 +0800

    add cachedimage

--- src/GiftedAvatar.js
+++ src/GiftedAvatar.js
@@ -3,6 +3,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { CachedImage } from 'react-native-cached-image';
 import Color from './Color';
 
 const { carrot, emerald, peterRiver, wisteria, alizarin, turquoise, midnightBlue } = Color;
@@ -39,14 +40,14 @@ export default class GiftedAvatar extends React.PureComponent {
       return this.props.user.avatar();
     } else if (typeof this.props.user.avatar === 'string') {
       return (
-        <Image
+        <CachedImage
           source={{ uri: this.props.user.avatar }}
           style={[styles.avatarStyle, this.props.avatarStyle]}
         />
       );
     } else if (typeof this.props.user.avatar === 'number') {
       return (
-        <Image
+        <CachedImage
           source={this.props.user.avatar}
           style={[styles.avatarStyle, this.props.avatarStyle]}
         />
